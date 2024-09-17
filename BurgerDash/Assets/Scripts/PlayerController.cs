4f76b431467b81bc48048628c359435973cba9a9
Ben Smith <ben.c.smith95@gmail.com>
2024-09-17 11:01:40 -0400

    Updated level one and started creating of level two. Added UI for score and lives and the logic behind score and live tracking

--- BurgerDash/Assets/Scripts/PlayerController.cs
+++ BurgerDash/Assets/Scripts/PlayerController.cs
@@ -2,31 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-  private const int SPEED = 8;
+  private const int SPEED = 7;
   private const int JUMP_POWER = 5;
-  private const float FALL_LIMIT = -8.0f;
+  private const float FALL_LIMIT = -6.0f;
+  private const float LEFT_LIMIT = -10.0f;
   private UnityEngine.Vector2 movement;
   private UnityEngine.Vector3 startingPosition;
   private Rigidbody2D rigidBody;
   private bool isJumping;
+  private int score;
+  private int lives;
 
   public LayerMask groundLayer;
+  public TMP_Text scoreText;
+  public TMP_Text livesText;
 
   private void Start()
   {
     rigidBody = GetComponent<Rigidbody2D>();
     startingPosition = transform.position;
     isJumping = false;
+    score = 0;
+    lives = 3;
   }
 
   private void Update()
   {
     // Reset to beginning if player falls off the map
     if (transform.position.y <= FALL_LIMIT)
+    {
+      transform.position = startingPosition;
+      rigidBody.velocity = UnityEngine.Vector2.zero;
+    }
+    if (transform.position.x <= LEFT_LIMIT)
     {
       transform.position = startingPosition;
       rigidBody.velocity = UnityEngine.Vector2.zero;
@@ -52,6 +65,31 @@ public class PlayerController : MonoBehaviour
     {
       isJumping = false;
       rigidBody.AddForce(UnityEngine.Vector2.up * JUMP_POWER, ForceMode2D.Impulse);
+    }
+  }
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.CompareTag("Burger"))
+    {
+      Destroy(collision.gameObject);
+      score++;
+      scoreText.text = "Score: " + score;
+    }
+    if (collision.CompareTag("Enemy"))
+    {
+      transform.position = startingPosition;
+      rigidBody.velocity = UnityEngine.Vector2.zero;
+      lives--;
+      livesText.text = "Lives: " + lives;
+
+      if (lives == 0)
+      {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("LoseScreen");
+      }
+    }
+    if (collision.CompareTag("Door") && score >= 4)
+    {
+      UnityEngine.SceneManagement.SceneManager.LoadScene("LevelTwo");
     }
   }
 }
