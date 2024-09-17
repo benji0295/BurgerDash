d3455e1b4dd5521e6a61276a4b7975fbc93b0e25
Ben Smith <ben.c.smith95@gmail.com>
2024-09-17 16:03:50 -0400

    updated design

---
+++ BurgerDash/Assets/Scripts/LoseScreen.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseScreen : MonoBehaviour
+{
+  private const float FALL_LIMIT = -8.0f;
+  private Vector3 startingPosition;
+  private Rigidbody2D rigidBody;
+
+  void Start()
+  {
+    rigidBody = GetComponent<Rigidbody2D>();
+    startingPosition = transform.position;
+  }
+
+  void Update()
+  {
+    if (transform.position.y <= FALL_LIMIT)
+    {
+      transform.position = startingPosition;
+      rigidBody.velocity = Vector2.zero;
+    }
+
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+  }
+}
